3dd3985e02945d17c40d385f9ea9209d81f6a7b0
Maksim <s1mplexgod1337@gmail.com>
2022-12-04 21:12:13 +0200

    add set budget parn

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ let totalAmount = document.getElementById('total-amount')
 let userAmount = document.getElementById('user-amount')
 
 const checkAmountButton = document.getElementById('check-amount')
-const totalAmountButton = document.getElementById('total-amoount-button')
+const totalAmountButton = document.getElementById('total-amount-button')
 const productTitle = document.getElementById('product-title')
 const errorMessage = document.getElementById('budget-error')
 const productTitleError = document.getElementById('product-title-error')
+const productCostError = document.getElementById('product-cost-error')
 const amount = document.getElementById('amount')
 const expenditureValue = document.getElementById('expenditure-value')
-const balanceValue = document.getElementById('calance-amount')
+const balanceValue = document.getElementById('balance-amount')
 const list = document.getElementById('list')
 let tempAmount = 0
+
+// set Budget Part
+totalAmountButton.addEventListener('click', () => {
+	tempAmount = totalAmount.value
+	// if empty or negative value
+	if (tempAmount === '' || tempAmount < 0) {
+		errorMessage.classList.remove('hide')
+	} else {
+		errorMessage.classList.add('hide')
+		// set Budget
+		amount.innerHTML = tempAmount
+		// set Balance
+		balanceValue.innerText = tempAmount - expenditureValue.innerText
+		// clear input
+		totalAmount.value = ''
+	}
+})
+// set Budget Part
